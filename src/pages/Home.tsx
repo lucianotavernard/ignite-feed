2d0b8392de92cf06c4c8cfb334b1107bb25c1433
Luciano Tavernard <lucianotavernard@gmail.com>
2022-07-09 16:22:16 -0300

    refactor: adjust home page component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from 'react'
+
+import { api } from '../services/api'
+
 import { Post } from '../components/Post'
 import { Header } from '../components/Header'
 import { SideBar } from '../components/SideBar'
 
+type Post = {
+  id: number
+  author: {
+    name: string
+    role: string
+    avatarUrl: string
+  }
+  content: Array<{ type: string; content: string }>
+  publishedAt: string
+}
+
 export function Home() {
+  const [posts, setPosts] = useState<Array<Post>>([])
+
+  useEffect(() => {
+    api.get('posts').then((response) => {
+      setPosts(response.data)
+    })
+  }, [])
+
   return (
     <div className="w-100 min-h-screen bg-[#121214]">
       <Header />
@@ -11,7 +34,15 @@ export function Home() {
         <SideBar />
 
         <section className="w-full md:flex-1">
-          <Post />
+          {posts.map((post) => (
+            <Post
+              id={post.id}
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </section>
       </main>
     </div>
