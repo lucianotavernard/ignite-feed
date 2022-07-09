5d0a418f24450494a1716defc2b8cf44361c15af
Luciano Tavernard <lucianotavernard@gmail.com>
2022-07-09 16:20:33 -0300

    refactor: adjust comment component

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,51 +1,150 @@
+import { useEffect, useMemo, useState } from 'react'
+
 import { ThumbsUp, Trash } from 'phosphor-react'
 
+import { format, formatDistanceToNow, parseISO } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+import { api } from '../services/api'
+
 import { Avatar } from './Avatar'
+import { Modal } from './Modal'
 
-export function Comment() {
+type CommentProps = {
+  comment: {
+    id: number
+    author: {
+      name: string
+      role: string
+      avatarUrl: string
+    }
+    content: string
+    publishedAt: string
+  }
+  onDelete: (id: number) => void
+}
+
+export function Comment({ comment, onDelete }: CommentProps) {
+  const { id, author, content, publishedAt } = comment
+
+  const parsedPublishedAt = parseISO(publishedAt)
+
+  const [totalApplauses, setTotalApplauses] = useState<number>(0)
+  const [modalVisible, setModalVisible] = useState(false)
+
+  async function handleCreateNewApplause() {
+    const createApplause = {
+      publishedAt: new Date().toISOString(),
+      commentId: id,
+      author
+    }
+
+    await api.post('applauses', createApplause)
+
+    setTotalApplauses((prevTotalApplauses) => prevTotalApplauses + 1)
+  }
+
+  function handleRemoveApplause() {
+    onDelete(id)
+  }
+
+  const publishedDateFormatted = useMemo(
+    () =>
+      format(parsedPublishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR
+      }),
+    [parsedPublishedAt]
+  )
+
+  const publishedDateRelativeToNow = useMemo(
+    () =>
+      formatDistanceToNow(parsedPublishedAt, {
+        locale: ptBR,
+        addSuffix: true
+      }),
+    [parsedPublishedAt]
+  )
+
+  useEffect(() => {
+    api.get('applauses').then((response) => {
+      setTotalApplauses(response.data.length)
+    })
+  }, [])
+
   return (
-    <div className="flex items-start gap-4 mt-4">
-      <Avatar
-        src="https://avatars.githubusercontent.com/u/19474041?v=4"
-        alt="Luciano Tavernard"
-      />
+    <>
+      <div className="flex items-start gap-4 mt-4">
+        <Avatar hasBorder src={author.avatarUrl} alt={author.name} />
 
-      <div className="flex-1">
-        <div className="p-4 rounded-lg bg-[#323238]">
-          <header className="flex items-start justify-between">
-            <div className="flex flex-col">
-              <strong className="block font-bold text-sm text-[#E1E1E6]">
-                Luciano Tavernard
-              </strong>
+        <div className="flex-1">
+          <div className="p-4 rounded-lg bg-[#323238]">
+            <header className="flex items-start justify-between">
+              <div className="flex flex-col">
+                <strong className="block font-bold text-sm text-[#E1E1E6]">
+                  {author.name}
+                </strong>
 
-              <time
-                title="06 de Junho de 2022 às 15h30"
-                dateTime="2022-06-06 15:33:00"
-                className="text-xs text-[#8D8D99]"
+                <time
+                  title={publishedDateFormatted}
+                  dateTime={parsedPublishedAt.toISOString()}
+                  className="text-xs text-[#8D8D99]"
+                >
+                  {publishedDateRelativeToNow}
+                </time>
+              </div>
+
+              <button
+                title="Deletar comentário"
+                onClick={() => setModalVisible(true)}
               >
-                Cerca de 1h atrás
-              </time>
-            </div>
+                <Trash size={24} className="text-[#8D8D99] hover:opacity-80" />
+              </button>
+            </header>
 
-            <button title="Deletar comentário">
-              <Trash size={24} className="text-[#8D8D99] hover:opacity-80" />
+            <p className="mt-4 text-sm text-[#C4C4CC]">{content}</p>
+          </div>
+
+          <footer className="mt-4">
+            <button
+              onClick={() => handleCreateNewApplause()}
+              className="flex items-center font-bold text-sm text-[#8D8D99] hover:text-[#00875F]"
+            >
+              <ThumbsUp size={20} />
+              <span className="ml-2">Aplaudir</span>
+              <span className="ml-1">•</span>
+              <span className="ml-1">{totalApplauses}</span>
             </button>
-          </header>
-
-          <p className="mt-4 text-sm text-[#C4C4CC]">
-            Muito bom Devon, parabéns!!
-          </p>
+          </footer>
         </div>
+      </div>
 
-        <footer className="mt-4">
-          <button className="flex items-center font-bold text-sm text-[#8D8D99] hover:text-[#00875F]">
-            <ThumbsUp size={20} />
-            <span className="ml-2">Aplaudir</span>
-            <span className="ml-1">•</span>
-            <span className="ml-1">01</span>
+      <Modal visible={modalVisible}>
+        <h1 className="font-bold text-2xl text-[#E1E1E6]">
+          Excluir comentário
+        </h1>
+
+        <p className="mt-6 text-base text-center text-[#C4C4CC]">
+          Você tem certeza que gostaria de <br /> excluir este comentário?
+        </p>
+
+        <footer className="mt-6">
+          <button
+            title="Cancelar remoção"
+            className="py-3 px-8 rounded font-bold text-sm text-white hover:opacity-80"
+            onClick={() => setModalVisible(false)}
+          >
+            Cancelar
+          </button>
+
+          <button
+            title="Confirmar remoção"
+            className="py-3 px-8 rounded bg-[#29292E] font-bold text-sm text-[#F75A68] hover:opacity-80"
+            onClick={() => handleRemoveApplause()}
+          >
+            Sim, excluir
           </button>
         </footer>
-      </div>
-    </div>
+      </Modal>
+    </>
   )
 }
