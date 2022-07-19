a4811ce45baefa10662ae9f19c3f82c3517b0cc6
Luciano Tavernard <lucianotavernard@gmail.com>
2022-07-18 23:16:16 -0300

    refactor: adjust avatar component styles

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -4,8 +4,8 @@ type AvatarProps = Pick<HTMLImageElement, 'src' | 'alt'> & {
 
 export function Avatar({ hasBorder, ...rest }: AvatarProps) {
   const className = hasBorder
-    ? 'w-14 p-0.5 rounded-lg border-2 border-[#00875F]'
-    : 'w-14 p-0.5 rounded-lg'
+    ? 'w-14 h-14 p-0.5 rounded-lg border-2 border-[#00875F]'
+    : 'w-14 h-14 p-0.5 rounded-lg'
 
   return <img className={className} {...rest} />
 }
